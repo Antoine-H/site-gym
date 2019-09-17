6ea66292b6973e2e003aad732156d1e3d811f65e
Antoine Huchet <git.eprok@spamgourmet.com>
2019-09-17 22:31:41 +0200

    Commented out DEFAULT_PAGINATION

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,34 +13,33 @@ PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
 DEFAULT_LANG = 'fr'
-
 DEFAULT_DATE_FORMAT = ('%d %B %Y')
 
-DEFAULT_PAGINATION = False
+#DEFAULT_PAGINATION = False
 
 THEME = "theme"
 
 MENUITEMS = (
     ('Accueil', '/index.html', 'mobile'),
-#    ('Le club', '/index.html#club', 'wide'),
     ('Actualités', '/blog.html', 'mobile'),
     ('Informations', '/infos.html', 'mobile'),
-#    ('Galerie', '/index.html', 'mobile'),
     ('Contact', '/index.html#infospratiques', 'wide'),
 )
 
 DISPLAY_PAGES_ON_MENU = False
 DISPLAY_CATEGORIES_ON_MENU = False
+
 DIRECT_TEMPLATES = ['index', 'blog']
+
 STATIC_PATHS = ['images',
                 'assets',
-                #'extra/CNAME',
                 'files',
                 'static/robots.txt']
+
 EXTRA_PATH_METADATA = {
-#    'extra/CNAME': {'path': 'CNAME'},
     'static/robots.txt': {'path': 'robots.txt'},
 }
+
 PLUGIN_PATHS = ["plugins", ]
 PLUGINS = ["include_assets", "sitemap", ]
 
@@ -51,5 +50,5 @@ SITEMAP = {
 #ARTICLE_EXCLUDES = ['documents', ]
 #PAGE_EXCLUDES = ['documents', ]
 
-RELATIVE_URLS = True
+#RELATIVE_URLS = True
 
